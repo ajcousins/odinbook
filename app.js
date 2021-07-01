d308b2fddf10f31598daa7985914aab394d8386c
Alvin Cousins <alvin.cousins@googlemail.com>
2021-07-01 19:51:14 +0100

    Deploy

--- app.js
+++ app.js
@@ -32,10 +32,7 @@ app.use((req, res, next) => {
 });
 
 app.use(function (req, res, next) {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://ajcousins.github.io/odinbook-react/"
-  ); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "https://ajcousins.github.io"); // update to match the domain you will make the request from
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
