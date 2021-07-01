a50043c782d3a0519c8d2bebb9d6501149958257
Alvin Cousins <alvin.cousins@googlemail.com>
2021-07-01 20:11:37 +0100

    Deploy

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.use("/api/v1/tweets", tweetRouter);
 // Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static("public/build"));
+  app.use(express.static("/public/build"));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "public", "build", "index.html"));
